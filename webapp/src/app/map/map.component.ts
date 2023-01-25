3a010e5bd665b73310a83fcf1f470000f21645f3
r.michau <r.michau@criteo.com>
2023-01-25 18:09:34 +0100

    Add color on icons depending of the velib arrival rate

--- webapp/src/app/map/map.component.ts
+++ webapp/src/app/map/map.component.ts
@@ -18,12 +18,39 @@ export class MapComponent implements OnInit {
   private long: number = 0
   private currentPostMarker: L.Layer | undefined
   private markers: L.Layer[] = [];
-  private greenIcon = L.icon({
+  private userIcon = L.icon({
     iconUrl: 'https://cdn.shopify.com/s/files/1/1061/1924/products/Very_Angry_Emoji_7f7bb8df-d9dc-4cda-b79f-5453e764d4ea_large.png?v=1571606036',
 
     iconSize: [38, 50], // size of the icon
   });
   dist = 1000;
+  private greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+  private orangeIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+  private redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+
 
   constructor(private mapService: MapService, private route: ActivatedRoute, private router: Router) {
   }
@@ -58,22 +85,24 @@ export class MapComponent implements OnInit {
     })
   };
 
-  toStringA(station: Station, dow: number): string {
+  toStringA(station: Station, dow: number): {txt: string, nbBikes: number} {
     const arrivals = station.Arrival
     let currentHour = new Date().getHours();
     let startHour = currentHour - 2;
     let endHour = currentHour + 2;
-    let res = `<a href="https://www.google.com/maps/search/?api=1&query=${station.Latitude},${station.Longitude}">Google Maps</a> </br>`
+    let nbBikes = 0;
+    let txt = `<a href="https://www.google.com/maps/search/?api=1&query=${station.Latitude},${station.Longitude}">Google Maps</a> </br>`
     for (var hour in arrivals[dow]) {
       let hourNumber = Number(hour);
       if (hourNumber == currentHour) {
-        res += `<b>${hour}h: ${arrivals[dow][hour]} bike/h</b></br>`
+        nbBikes = arrivals[dow][hour];
+        txt += `<b>${hour}h: ${arrivals[dow][hour]} bike/h</b></br>`
       }
       if (hourNumber > startHour && hourNumber < endHour) {
-        res += `${hour}h: ${arrivals[dow][hour]} bike/h</br>`
+        txt += `${hour}h: ${arrivals[dow][hour]} bike/h</br>`
       }
     }
-    return res
+    return {txt, nbBikes}
   }
 
   resetLocation() {
@@ -85,12 +114,15 @@ export class MapComponent implements OnInit {
   }
 
   updateMap() {
+
+
+
     this.router.navigate([], {queryParams: {dist: this.dist, lat: this.lat, long: this.long }, relativeTo: this.route});
     this.map.setView([this.lat, this.long])
     if (this.currentPostMarker != undefined) {
       this.map.removeLayer(this.currentPostMarker);
     }
-    this.currentPostMarker = L.marker([this.lat, this.long], {icon: this.greenIcon}).addTo(this.map);
+    this.currentPostMarker = L.marker([this.lat, this.long], {icon: this.userIcon}).addTo(this.map);
     let dow = this.getDow()
     this.markers.forEach(marker => {
       this.map.removeLayer(marker);
@@ -107,8 +139,15 @@ export class MapComponent implements OnInit {
       }
       coordinates.forEach(station => {
         let coordinates: [number, number] = [station.Latitude, station.Longitude]
-        const marker = L.marker(coordinates).addTo(this.map);
-        marker.bindPopup(this.toStringA(station, dow)).openPopup();
+        const stationDetails = this.toStringA(station, dow)
+        let icon = this.redIcon;
+        if(stationDetails.nbBikes >= 7) {
+          icon = this.greenIcon
+        } else if(stationDetails.nbBikes >= 5) {
+          icon = this.orangeIcon
+        }
+        const marker = L.marker(coordinates, {icon: icon}).addTo(this.map);
+        marker.bindPopup(stationDetails.txt).openPopup();
         this.markers.push(marker);
       });
     });
